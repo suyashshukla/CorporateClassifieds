22afd37fc5acf57bd3ec173cdceee98cd9c81b62
suyashshukla <suyashshukla1997@gmail.com>
2019-10-15 18:46:45 +0530

    Content Pane

---
+++ src/app/View/View.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+selector: 'View-component',
+templateUrl: './View.component.html',
+styleUrls: ['./View.component.css']
+})
+export class ViewComponent implements OnInit {
+
+constructor() {
+}
+
+ngOnInit(){
+
+}
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path : '', component : ViewComponent}
+  ];
+
+import { ViewComponent } from './View/View.component';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,{useHash : true})],
