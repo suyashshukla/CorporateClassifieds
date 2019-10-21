81339830d92c6c15380765d163a9a0c3fc736dac
suyashshukla <suyashshukla1997@gmail.com>
2019-10-21 10:29:50 +0530

    Adding Stuff

--- CorporateClassifieds/website/main.js
+++ CorporateClassifieds/website/main.js
@@ -58,7 +58,7 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony default export */ __webpack_exports__["default"] = ("Inbox\r\n");
+/* harmony default export */ __webpack_exports__["default"] = ("Inbox\r\nOutbox\r\n");
 
 /***/ }),
 
